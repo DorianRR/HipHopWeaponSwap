0b9c39ed3ae8bfe9901939e03d4714b837522994
Utkarsh Rao Viswavasu <hootkarsh@gmail.com>
2018-01-26 22:42:07 -0700

    Preliminary enemy ai
    Enemey moves in a random direction for a specified length, then stops and fires a set number of bullets at a set interval.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject projectile;
+    //public GameObject playerOne;
+    //public GameObject playerTwo;
+    // Use this for initialization
+    public float walkSpeed;
+    public float wanderRange;
+    public float shootInterval;
+    public int numberShots;
+
+    private bool isShooting = false;
+    private enum State { stand, shoot, walk, pending };
+    private State currState;
+    private int shotsFired = 0;
+
+    void Start () {
+        currState = State.walk;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonUp(0))
+        {
+            fire(Vector3.right);
+        }
+
+        if (currState == State.walk)
+        {
+            StartCoroutine("handleWalking");
+        }
+        else if (currState == State.shoot)
+        {
+            StartCoroutine("handleShooting");
+        }
+        else if (currState == State.stand)
+        {
+            StartCoroutine("handleStanding");
+        }
+        else if (currState == State.pending)
+        {
+
+        }
+        
+	}
+
+
+    IEnumerator handleShooting()
+    {
+        currState = State.pending;
+
+        fire(Vector3.right);
+
+        if ( shotsFired < numberShots)
+        {
+            shotsFired++;
+            currState = State.stand;
+        }
+        else
+        {
+            currState = State.walk;
+        }
+
+        yield return null;
+    }
+
+    IEnumerator handleStanding()
+    {
+        currState = State.pending;
+
+        yield return new WaitForSeconds(shootInterval);
+
+        currState = State.shoot;
+    }
+
+    IEnumerator handleWalking()
+    {
+        currState = State.pending;
+        
+        float bufferDistance = walkSpeed * .5f;
+        bool foundValidPos = false;
+        Vector3 target = new Vector3();
+        while (!foundValidPos)
+        {
+            Vector3 offset = new Vector3(Random.Range(-1, 1), 0, Random.Range(-1, 1)).normalized;
+            offset *= wanderRange;
+            target = offset +transform.position;
+            if (Physics.OverlapSphere(target, 0.1f).Length < 1)
+                foundValidPos = true;
+        }
+
+        while (Vector3.Distance(target, transform.position) > bufferDistance)
+        {
+            Vector3 newPos = transform.position;
+            newPos += (target - transform.position).normalized * walkSpeed * Time.deltaTime;
+            transform.position = newPos;
+
+            yield return null;
+        }
+
+        currState = State.stand;
+        shotsFired = 0;
+    }
+
+    void fire(Vector3 fireDirection)
+    {
+        Projectile newProjectile = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Projectile>();
+        newProjectile.setDirection(fireDirection);
+    }
+
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    private Vector3 direction;
+    public float speed;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPosition = transform.position;
+        newPosition += direction * speed * Time.deltaTime;
+        transform.position = newPosition;
+	}
+
+    public void setDirection(Vector3 newDirection)
+    {
+        direction = newDirection.normalized;
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "WorldBounds")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
