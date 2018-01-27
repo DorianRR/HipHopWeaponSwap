ced7352fdf7544ae4cf497e171a1b42048ecf15d
Dorian Rosen <dorianrrosen@gmail.com>
2018-01-27 14:39:18 -0700

    merging update

--- Assets/Scripts/Player/PlayerController2.cs
+++ Assets/Scripts/Player/PlayerController2.cs
@@ -1,28 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController2 : MonoBehaviour {
 
     public Projectile projectile;
-    public Slider healthSlider;
-    bool isYelling = false;
-    public bool hasLiquor;
-    public bool isShotgun;
-    public float moveSpeed = 15f;
-
+    public Colour weaponColour;
 
     private Vector3 direction;
     private Quaternion rotation;
-    private float health;
-
 
     // Use this for initialization
     void Start ()
     {
-        direction = new Vector3(0, 0, 1);
-        health = 100f;
+        direction = new Vector3(1, 0, 0);
     }
 
     // Update is called once per frame
@@ -36,50 +27,22 @@ public class PlayerController2 : MonoBehaviour {
             rotation = Quaternion.LookRotation(direction);
             transform.rotation = rotation;
         }
-
-        //direction = new Vector3(Input.GetAxis("RightStickX_P2"), 0, Input.GetAxis("RightStickY_P2"));
-        //direction.Normalize();
-
-        if (Input.GetAxis("Horizontal_P2") > 0 || Input.GetAxis("Horizontal_P2") < 0 || Input.GetAxis("Vertical_P2") < 0 || Input.GetAxis("Vertical_P2") > 0)
-        {
-            gameObject.GetComponent<Rigidbody>().constraints = 
-                RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
-
-            gameObject.GetComponent<Rigidbody>().position = gameObject.GetComponent<Rigidbody>().position + moveSpeed * Time.deltaTime * (new Vector3(Input.GetAxis("Horizontal_P2"), 0, 0));
-            gameObject.GetComponent<Rigidbody>().position = gameObject.GetComponent<Rigidbody>().position + moveSpeed * Time.deltaTime * (new Vector3(0, 0, Input.GetAxis("Vertical_P2")));
-
-        }
-        else
-        {
-            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
-        }
+        direction = new Vector3(Input.GetAxis("RightStickX_P2"), 0, Input.GetAxis("RightStickY_P2"));
+        direction.Normalize();
+        gameObject.GetComponent<Rigidbody>().position = gameObject.GetComponent<Rigidbody>().position + 0.5f * (new Vector3(Input.GetAxis("Horizontal_P2"), 0, 0));
+        gameObject.GetComponent<Rigidbody>().position = gameObject.GetComponent<Rigidbody>().position + 0.5f * (new Vector3(0, 0, Input.GetAxis("Vertical_P2")));
 
         if (Input.GetAxisRaw("3rd axis P_2") > 0)
         {
             Shoot(direction);
-        }
-        healthSlider.value = health;
-        if (Input.GetButtonDown("Yell_P2"))
-        {
-            StartCoroutine(Yell());
-        }
+        }   
     }
 
     void Shoot(Vector3 shootDir)
     {
         Projectile newProjectile = Instantiate(projectile, (transform.position + (5* shootDir)), Quaternion.identity).GetComponent<Projectile>();
+        newProjectile.setColour(weaponColour);
         newProjectile.setDirection(shootDir);
     }
 
-    public void getHit()
-    {
-        health -= 24f;
-    }
-
-    IEnumerator Yell()
-    {
-        isYelling = true;
-        yield return new WaitForSeconds(2f);
-        isYelling = false;
-    }
 }
--- Assets/Scripts/playerShoot.cs
+++ Assets/Scripts/playerShoot.cs
@@ -2,51 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Colour { Red, Blue };
-
-public class Enemy : MonoBehaviour {
+public class playerShoot : MonoBehaviour
+{
 
     public GameObject projectile;
     private GameObject projectileHolder;
+    public GameObject playerOne;
     //public GameObject playerTwo;
     // Use this for initialization
     public float walkSpeed;
     public float wanderRange;
     public float shootInterval;
     public int numberShots;
-    public Material redMaterial;
-    public Material blueMaterial;
-    public int hitPoints;
-    public Vector3 target;
-    
-
     private Vector3 playerPosition;
+
     private bool isShooting = false;
     private enum State { stand, shoot, walk, pending };
     private State currState;
     private int shotsFired = 0;
-    public Colour thisColour;
-    private GameController gc;
-    private GameObject targetPlayer;
-    private GameObject originalPlayer;
 
-
-    void Start () {
+    void Start()
+    {
         currState = State.walk;
         projectileHolder = GameObject.Find("Projectiles");
-        playerPosition = targetPlayer.transform.position;
-        target.y = transform.position.y;
-
-        if (thisColour == Colour.Red)
-            GetComponent<Renderer>().material = redMaterial;
-        else if (thisColour == Colour.Blue)
-            GetComponent<Renderer>().material = blueMaterial;
+        playerPosition = playerOne.transform.position;
     }
 
     // Update is called once per frame
-    void Update () {
-        playerPosition = targetPlayer.transform.position;
-
+    void Update()
+    {
+        playerPosition = playerOne.transform.position;
         if (Input.GetMouseButtonUp(0))
         {
             fire(Vector3.right);
@@ -68,8 +53,8 @@ public class Enemy : MonoBehaviour {
         {
 
         }
-        
-	}
+
+    }
 
 
     IEnumerator handleShooting()
@@ -77,7 +62,7 @@ public class Enemy : MonoBehaviour {
         currState = State.pending;
 
 
-        if ( shotsFired < numberShots)
+        if (shotsFired < numberShots)
         {
             //fire(Vector3.right);
             fireAtPlayer();
@@ -86,7 +71,6 @@ public class Enemy : MonoBehaviour {
         }
         else
         {
-            moveRandomly();
             currState = State.walk;
         }
 
@@ -106,9 +90,26 @@ public class Enemy : MonoBehaviour {
     {
         currState = State.pending;
 
-        float bufferDistance = walkSpeed * .1f;
+        float bufferDistance = walkSpeed * .5f;
+        bool foundValidPos = false;
+        Vector3 target = new Vector3();
+        while (!foundValidPos)
+        {
+            Vector3 offset = new Vector3(Random.Range(-1, 1), 0, Random.Range(-1, 1)).normalized;
+            offset *= wanderRange;
+            target = offset + transform.position;
 
-
+            if (!Physics.Raycast(transform.position, offset, wanderRange))
+                foundValidPos = true;
+            else
+            {
+                offset.x *= -1;
+                offset.z *= -1;
+                target = offset + transform.position;
+                if (!Physics.Raycast(transform.position, offset, wanderRange))
+                    foundValidPos = true;
+            }
+        }
         while (Vector3.Distance(target, transform.position) > bufferDistance)
         {
             Vector3 newPos = transform.position;
@@ -133,56 +134,15 @@ public class Enemy : MonoBehaviour {
         Projectile newProjectile = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Projectile>();
         newProjectile.transform.SetParent(projectileHolder.transform);
         Vector3 direction = playerPosition - transform.position;
-        direction.y = 0;
+        direction.y = transform.position.y;
         newProjectile.setDirection(direction);
     }
 
     //Hit by projectile
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == thisColour.ToString())
-        {
-            hitPoints--;
-            Destroy(collision.gameObject);
-        }
-        if(hitPoints<1)
-        {
-            gc.registerEnemyDead(originalPlayer);
-            Destroy(this.gameObject);
-        }
-    }
-    private void moveRandomly()
-    {
-        bool foundValidPos = false;
-        while (!foundValidPos)
-        {
-            Vector3 offset = new Vector3(Random.Range(-1, 1), 0, Random.Range(-1, 1)).normalized;
-            offset *= wanderRange;
-            target = offset + transform.position;
-
-            if (!Physics.Raycast(transform.position, offset, wanderRange + GetComponent<Collider>().bounds.size.x))
-                foundValidPos = true;
-            else
-            {
-                offset.x *= -1;
-                offset.z *= -1;
-                target = offset + transform.position;
-                if (!Physics.Raycast(transform.position, offset, wanderRange + GetComponent<Collider>().bounds.size.x ))
-                    foundValidPos = true;
-            }
-        }
-
+       
     }
 
-    public void setInitialMovement(Vector3 initPosition)
-    {
-        target = initPosition;
-    }
 
-    public void setEnemyInfo(GameController gc, GameObject player)
-    {
-        this.gc = gc;
-        this.targetPlayer = player;
-        this.originalPlayer = player;
-    }
 }
