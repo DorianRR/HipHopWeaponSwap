a4082782acb1dff0be00596d0bdda9df2ce77f8e
Utkarsh Rao Viswavasu <hootkarsh@gmail.com>
2018-01-27 17:21:20 -0700

    misc

--- Assets/Scripts/Player/PlayerController2.cs
+++ Assets/Scripts/Player/PlayerController2.cs
@@ -1,25 +1,47 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerController2 : MonoBehaviour {
+public class PlayerController2 : MonoBehaviour
+{
 
     public Projectile projectile;
-    public Colour weaponColour;
+    public Slider healthSlider;
+    public bool isYelling = false;
+    public bool hasLiquor;
+    public bool isShotgun;
+    public float moveSpeed = 15f;
+    public Colour weaponColor;
+
+
+
 
     private Vector3 direction;
     private Quaternion rotation;
+    private float health;
+    private bool canShoot = true;
+    private float timeRest = 1.2f;
+    private float burstTime = 0.05f;
+    private float time = 0;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-        direction = new Vector3(1, 0, 0);
+        direction = new Vector3(0, 0, 1);
+        health = 100f;
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
-
+        time += Time.deltaTime;
+        if (time >= timeRest)
+        {
+            canShoot = true;
+            time = 0;
+        }
         if (Input.GetAxis("RightStickX_P2") > 0 || Input.GetAxis("RightStickY_P2") > 0 || Input.GetAxis("RightStickX_P2") < 0 || Input.GetAxis("RightStickY_P2") < 0)
         {
             direction = new Vector3(Input.GetAxis("RightStickX_P2"), 0, Input.GetAxis("RightStickY_P2"));
@@ -27,22 +49,72 @@ public class PlayerController2 : MonoBehaviour {
             rotation = Quaternion.LookRotation(direction);
             transform.rotation = rotation;
         }
-        direction = new Vector3(Input.GetAxis("RightStickX_P2"), 0, Input.GetAxis("RightStickY_P2"));
-        direction.Normalize();
-        gameObject.GetComponent<Rigidbody>().position = gameObject.GetComponent<Rigidbody>().position + 0.5f * (new Vector3(Input.GetAxis("Horizontal_P2"), 0, 0));
-        gameObject.GetComponent<Rigidbody>().position = gameObject.GetComponent<Rigidbody>().position + 0.5f * (new Vector3(0, 0, Input.GetAxis("Vertical_P2")));
+
+        //direction = new Vector3(Input.GetAxis("RightStickX_P2"), 0, Input.GetAxis("RightStickY_P2"));
+        //direction.Normalize();
+
+        if (Input.GetAxis("Horizontal_P2") > 0 || Input.GetAxis("Horizontal_P2") < 0 || Input.GetAxis("Vertical_P2") < 0 || Input.GetAxis("Vertical_P2") > 0)
+        {
+            gameObject.GetComponent<Rigidbody>().constraints =
+                RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+
+            gameObject.GetComponent<Rigidbody>().position = gameObject.GetComponent<Rigidbody>().position + moveSpeed * Time.deltaTime * (new Vector3(Input.GetAxis("Horizontal_P2"), 0, 0));
+            gameObject.GetComponent<Rigidbody>().position = gameObject.GetComponent<Rigidbody>().position + moveSpeed * Time.deltaTime * (new Vector3(0, 0, Input.GetAxis("Vertical_P2")));
+
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+        }
 
         if (Input.GetAxisRaw("3rd axis P_2") > 0)
         {
-            Shoot(direction);
-        }   
+            if (canShoot)
+            {
+                Shoot(direction, isShotgun);
+                canShoot = false;
+            }
+        }
+        healthSlider.value = health;
+        if (Input.GetButtonDown("Yell_P2"))
+        {
+            StartCoroutine(Yell());
+        }
     }
 
-    void Shoot(Vector3 shootDir)
+    void Shoot(Vector3 shootDir, bool isShotgun)
     {
-        Projectile newProjectile = Instantiate(projectile, (transform.position + (5* shootDir)), Quaternion.identity).GetComponent<Projectile>();
-        newProjectile.setColour(weaponColour);
-        newProjectile.setDirection(shootDir);
+        if (!isShotgun)
+        {
+            StartCoroutine(Burst(shootDir));
+        }
+        else
+        {
+
+        }
+
     }
 
+    public void getHit()
+    {
+        health -= 24f;
+    }
+
+    IEnumerator Yell()
+    {
+        isYelling = true;
+        yield return new WaitForSeconds(2f);
+        isYelling = false;
+    }
+
+    IEnumerator Burst(Vector3 shootDir)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Projectile newProjectile = Instantiate(projectile, (transform.position + (5 * shootDir)), Quaternion.identity).GetComponent<Projectile>();
+            newProjectile.setDirection(shootDir);
+            newProjectile.setColour(weaponColor);
+            yield return new WaitForSeconds(burstTime);
+        }
+    }
 }
